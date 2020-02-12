69079d1b9758eab2e3f2fd81d194cf10bc12444f
wangliang <236179666@qq.com>
2020-02-12 14:00:39 +0000

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,9 @@ use Illuminate\Http\Request;
 
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function(){
     Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function(){
+        // 图片验证码
+        Route::post('captchas', 'CaptchasController@store')
+            ->name('captchas.store');
         // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')
             ->name('verificationCodes.store');
@@ -28,7 +31,7 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function(){
     });
 
     Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function(){
-        
+
     });
 });
 
